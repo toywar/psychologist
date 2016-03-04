f63258bc2ec04dab4a3cd2673ffd05c9b1a61291
Anechka <anechka@anechka-mac.local>
2016-03-05 00:50:53 +0300

    add Popover

--- Psychologist/PsychologistViewController.swift
+++ Psychologist/PsychologistViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class PsychologistViewController: UIViewController {
     
+    @IBAction func nothing(sender: UIButton) {
+        performSegueWithIdentifier("nothing", sender: nil)
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let hvc = segue.destinationViewController as? HappinessViewController {
+        var destination = segue.destinationViewController as? UIViewController
+        if let navCon = destination as? UINavigationController {
+            destination = navCon.visibleViewController
+        }
+        if let hvc = destination as? HappinessViewController {
             if let identifier = segue.identifier {
                 switch identifier {
                     case "sad": hvc.happiness = 0
                     case "happy": hvc.happiness = 100
+                    case "nothing": hvc.happiness = 25
                 default: hvc.happiness = 50
                 }
             }
